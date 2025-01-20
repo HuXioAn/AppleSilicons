c348ffb0a0ceaff62e9b135496315ee737788576
Andong Hu <hu7006@outlook.com>
2025-01-20 22:26:12 +0100

    amend

--- control/config.py
+++ control/config.py
@@ -1,6 +1,6 @@
 import os
 
-sizes = [32, 64, 128]
+sizes = [32, 64, 128, 150, 256, 500, 512, 1000, 1024, 1500, 2000, 2048, 2500, 4096]
 # sizes = [2048, 4096, 8192, 16384, 32768, 65536]
 
 enablePureCPU = True # for large matrix, baseline and omp can be disabled by setting this to False
